161bec7946cdbd781c8b3fc80cccb4a38c151226
Michiel Sikkes <michiel@firmhouse.com>
2013-07-02 21:59:38 +0200

    Lol actually initialize our ssl_context so we don't segfault

--- ext/polarssl/ssl.c
+++ ext/polarssl/ssl.c
@@ -1,13 +1,17 @@
 #include "polarssl.h"
 #include "polarssl/ssl.h"
 #include "polarssl/ctr_drbg.h"
+#include "polarssl/net.h"
 #include "ruby/io.h"
 
-static VALUE R_ssl_set_endpoint();
 static VALUE R_ssl_allocate();
+static VALUE R_ssl_initialize();
+static VALUE R_ssl_set_endpoint();
 static VALUE R_ssl_set_authmode();
 static VALUE R_ssl_set_rng();
 static VALUE R_ssl_set_bio();
+static VALUE R_ssl_handshake();
+
 
 void Init_ssl()
 {
@@ -17,17 +21,29 @@ void Init_ssl()
   rb_define_const(cSSL, "SSL_VERIFY_NONE", INT2NUM(SSL_VERIFY_NONE));
 
   rb_define_alloc_func(cSSL, R_ssl_allocate);
+  rb_define_method(cSSL, "initialize", R_ssl_initialize, 0);
   rb_define_method(cSSL, "set_endpoint", R_ssl_set_endpoint, 1);
   rb_define_method(cSSL, "set_authmode", R_ssl_set_authmode, 1);
   rb_define_method(cSSL, "set_rng", R_ssl_set_rng, 1);
   rb_define_method(cSSL, "set_bio", R_ssl_set_bio, 4);
+  rb_define_method(cSSL, "handshake", R_ssl_handshake, 0);
 }
 
 static VALUE R_ssl_allocate(VALUE klass)
 {
   ssl_context *ssl;
 
-  return Data_Make_Struct(klass, ssl_context, 0, ssl_free, ssl);
+  return Data_Make_Struct(klass, ssl_context, 0, NULL, ssl);
+}
+
+static VALUE R_ssl_initialize(VALUE self)
+{
+  ssl_context *ssl;
+
+  Data_Get_Struct(self, ssl_context, ssl);
+  ssl_init(ssl);
+
+  return self;
 }
 
 static VALUE R_ssl_set_endpoint(VALUE self, VALUE endpoint_mode)
@@ -37,7 +53,8 @@ static VALUE R_ssl_set_endpoint(VALUE self, VALUE endpoint_mode)
   ssl_context *ssl;
   Data_Get_Struct(self, ssl_context, ssl);
 
-  ssl_set_endpoint(ssl, NUM2INT(endpoint_mode));
+  // ssl_set_endpoint(ssl, NUM2INT(endpoint_mode));
+  ssl_set_endpoint(ssl, SSL_IS_CLIENT);
 
   return self;
 }
@@ -49,7 +66,8 @@ static VALUE R_ssl_set_authmode(VALUE self, VALUE authmode)
   ssl_context *ssl;
   Data_Get_Struct(self, ssl_context, ssl);
 
-  ssl_set_authmode(ssl, NUM2INT(authmode));
+  // ssl_set_authmode(ssl, NUM2INT(authmode));
+  ssl_set_authmode(ssl, SSL_VERIFY_NONE);
 
   return self;
 }
@@ -81,6 +99,16 @@ static VALUE R_ssl_set_bio(VALUE self, VALUE recv_func, VALUE input_socket, VALU
   GetOpenFile(input_socket, fptr);
 
   ssl_set_bio(ssl, net_recv, &fptr->fd, net_send, &fptr->fd);
+
+  return self;
+}
+
+static VALUE R_ssl_handshake(VALUE self)
+{
+  ssl_context *ssl;
+  Data_Get_Struct(self, ssl_context, ssl);
+
+  ssl_handshake(ssl);
 
   return self;
 }
