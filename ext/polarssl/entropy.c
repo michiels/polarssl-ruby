11a3e80655f37af4a058a57de34bab04d7242ff1
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-08-14 19:51:50 +0200

    Add license comments to all C implementation files

--- ext/polarssl/entropy.c
+++ ext/polarssl/entropy.c
@@ -1,3 +1,26 @@
+/*
+ *  Wrapping code for the PolarSSL::Entropy class.
+ *
+ *  Copyright (C) 2013  Michiel Sikkes
+ *
+ *  This file is part of polarssl-ruby (http://github.com/michiels/polarssl-ruby)
+ *
+ *  All rights reserved.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "polarssl.h"
 #include "polarssl/entropy.h"
 #include "ruby.h"
