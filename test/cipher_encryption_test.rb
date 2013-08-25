be1abc8ae8f98f2004cb6e1ecc939ff364d7006d
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-08-25 19:45:27 +0200

    Added raw encrypted data to test on.

--- test/cipher_encryption_test.rb
+++ test/cipher_encryption_test.rb
@@ -12,10 +12,21 @@ class CipherTest < MiniTest::Unit::TestCase
     encrypted = cipher.finish
 
     encrypted_base64 = Base64.encode64(encrypted)
-    puts encrypted_base64
 
     assert !encrypted.nil?
     assert !encrypted_base64.nil?
+
+    should_encrypt_as = <<EOF
+nsDrK6JHaPXC8janRLIODtJfY8K6d+OfHQSSsdUSW/rDmzJ9GT1YeTllMPMA
+MSKpyxi3BT8mLY+sR/cC+ElxAS5GSiiqrZ/D99WcqgNHwNNeXgTwghWZX9w3
+rk+t6S5qwMs7aXoIZ+Hw/0Vb2IhLLcUA7V1ei3XHsdJ6XvVN8JhbuIkHrkVL
+Cxn/UlDdNXk23dORm878TyvQ9839FBT80zflSyYjsHCj43HxC6KBg7lPNeEz
+3swWwri2g2odFJojm0Mp0PS+bKFDgu+pXvGrCon5Fhy4nPJyFl73TvThF+w9
+2FRm/A7Vb5U75wJAzv6FQZQSwfsACVLWPpuHiR7fV7Uyc2VIV2yOpaI9d3Ix
+1neY3ANZhqZxWJ03MqrS/zzahJTJ16JbpyIvTTwoVoZWBe9ypuGO7uPrN7da
++kw=
+EOF
+  assert_equal should_encrypt_as, encrypted_base64
   end
 
 end
