edb879e1a3679445a6948b9c857860af157bfae6
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-08-25 17:48:15 +0200

    Added PolarSSL::Chiper.setkey() method that sets key

--- ext/polarssl/cipher.c
+++ ext/polarssl/cipher.c
@@ -26,13 +26,19 @@
 
 VALUE rb_cipher_allocate();
 VALUE rb_cipher_initialize();
+VALUE rb_cipher_setkey();
 
 void Init_cipher()
 {
   VALUE cCipher = rb_define_class_under( rb_mPolarSSL, "Cipher", rb_cObject);
 
+  rb_define_const( cCipher, "OPERATION_ENCRYPT", INT2NUM(POLARSSL_ENCRYPT) );
+  rb_define_const( cCipher, "OPERATION_DECRYPT", INT2NUM(POLARSSL_DECRYPT) );
+  rb_define_const( cCipher, "OPERATION_NONE", INT2NUM(POLARSSL_OPERATION_NONE) );
+
   rb_define_alloc_func( cCipher, rb_cipher_allocate );
   rb_define_method( cCipher, "initialize", rb_cipher_initialize, 1 );
+  rb_define_method( cCipher, "setkey", rb_cipher_setkey, 3 );
 }
 
 VALUE rb_cipher_allocate( VALUE klass )
@@ -54,4 +60,15 @@ VALUE rb_cipher_initialize( VALUE self, VALUE cipher_type )
   cipher_init_ctx(ctx, cipher_info_from_string(StringValueCStr(cipher_type)));
 
   return self;
+}
+
+VALUE rb_cipher_setkey( VALUE self, VALUE key, VALUE key_length, VALUE operation )
+{
+  cipher_context_t *ctx;
+
+  Data_Get_Struct( self, cipher_context_t, ctx );
+
+  cipher_setkey( ctx, (unsigned char *) StringValueCStr(key), FIX2INT(key_length), NUM2INT(operation) );
+
+  return Qtrue;
 }
--- test/cipher_encryption_test.rb
+++ test/cipher_encryption_test.rb
@@ -5,9 +5,9 @@ class CipherTest < MiniTest::Unit::TestCase
   def test_aes_128_ctr_encrypt
     cipher = PolarSSL::Cipher.new("AES-128-CTR")
 
-    # cipher.setkey("1234567890123456", 128)
-    # cipher.update("hallo")
-    # encrypted = cipher.finish
+    cipher.setkey("1234567890123456", 128, PolarSSL::Cipher::OPERATION_ENCRYPT)
+    cipher.update("hallo")
+    encrypted = cipher.finish
 
     assert_not_nil encrypted
   end
