fa1d6a333eea44d2289fa4e6795fb2faa75a9930
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-08-25 17:38:07 +0200

    Added allocate and initialize which sets cipher info from given string to PolarSSL::Cipher.new

--- ext/polarssl/cipher.c
+++ ext/polarssl/cipher.c
@@ -22,8 +22,39 @@
  */
 
  #include "polarssl.h"
+ #include "polarssl/cipher.h"
+
+VALUE rb_cipher_allocate();
+VALUE rb_cipher_initialize();
 
 void Init_cipher()
 {
   VALUE cCipher = rb_define_class_under( rb_mPolarSSL, "Cipher", rb_cObject);
+
+  rb_define_alloc_func( cCipher, rb_cipher_allocate );
+  rb_define_method( cCipher, "initialize", rb_cipher_initialize, 1 );
+}
+
+VALUE rb_cipher_allocate( VALUE klass )
+{
+  cipher_context_t *ctx;
+
+  ctx = ALLOC( cipher_context_t );
+  cipher_init_ctx(ctx, cipher_info_from_type(POLARSSL_CIPHER_NULL));
+
+  return Data_Wrap_Struct( klass, 0, cipher_free_ctx, ctx);
+}
+
+VALUE rb_cipher_initialize( VALUE self, VALUE cipher_type )
+{
+  cipher_context_t *ctx;
+  int ret;
+
+  Data_Get_Struct( self, cipher_context_t, ctx );
+
+  ret = cipher_init_ctx(ctx, cipher_info_from_string(StringValueCStr(cipher_type)));
+
+  printf("%d", ret);
+
+  return self;
 }
--- test/cipher_encryption_test.rb
+++ test/cipher_encryption_test.rb
@@ -3,11 +3,11 @@ require 'test_helper'
 class CipherTest < MiniTest::Unit::TestCase
 
   def test_aes_128_ctr_encrypt
-    cipher = PolarSSL::Cipher.new
+    cipher = PolarSSL::Cipher.new("AES-128-CTR")
 
-    cipher.setkey("1234567890123456", 128)
-    cipher.update("hallo")
-    encrypted = cipher.finish
+    # cipher.setkey("1234567890123456", 128)
+    # cipher.update("hallo")
+    # encrypted = cipher.finish
 
     assert_not_nil encrypted
   end
