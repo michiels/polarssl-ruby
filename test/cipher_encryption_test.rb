a1d66e013509145b7173e0f81681927701cc864f
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-09-15 18:27:13 +0200

    Refactor hashes and add comment to reference sample data

--- test/cipher_encryption_test.rb
+++ test/cipher_encryption_test.rb
@@ -5,30 +5,20 @@ require 'securerandom'
 class CipherTest < MiniTest::Unit::TestCase
 
   def test_aes_128_ctr_encrypt
-    cipher = PolarSSL::Cipher.new("AES-128-CTR")
-    
+    # These are hex-formatted strings that come from NIST Special Publication 800-38A 2001 Edition:
+    # Recommendation for Block Cipher Modes of Operation, Methods and Techniques by Morris Dworkin.
     key = hex_to_bin("2b7e151628aed2a6abf7158809cf4f3c")
     iv = hex_to_bin("f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff")
+    input = hex_to_bin("6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e51")
+    should_encrypt_as hex_to_bin("874d6191b620e3261bef6864990db6ce9806f66b7970fdff8617187bb9fffdff")
     
-    # TODO: use random generate iv
-    # iv = SecureRandom.random_bytes(16)
-    
+    cipher = PolarSSL::Cipher.new("AES-128-CTR")
     cipher.setkey(key, 128, PolarSSL::Cipher::OPERATION_ENCRYPT)
-    
     cipher.reset(iv)
-    
-    cipher.update(hex_to_bin("6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e51"))
-
+    cipher.update(input)
     encrypted = cipher.finish
 
-    encrypted_base64 = Base64.encode64(encrypted)
-
-    assert !encrypted.nil?
-    assert !encrypted_base64.nil?
-
-    should_encrypt_as = hex_to_bin("874d6191b620e3261bef6864990db6ce9806f66b7970fdff8617187bb9fffdff")
-    assert_equal should_encrypt_as, encrypted
-    
+    assert_equal should_encrypt_as, encrypted    
   end
 
   def test_unsupported_cipher
