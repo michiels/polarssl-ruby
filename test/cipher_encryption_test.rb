d67eb0ae6a8b8a7d10c3d12276ff44cfea0e9cf2
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-09-15 15:57:38 +0200

    Add reset() cipher call for IV

--- test/cipher_encryption_test.rb
+++ test/cipher_encryption_test.rb
@@ -1,12 +1,19 @@
 require 'test_helper'
 require 'base64'
-
+require 'securerandom'
 
 class CipherTest < MiniTest::Unit::TestCase
 
   def test_aes_128_ctr_encrypt
     cipher = PolarSSL::Cipher.new("AES-128-CTR")
-
+    
+    iv = "1234567890123456" # our known IV. 
+    
+    # TODO: use random generate iv
+    # iv = SecureRandom.random_bytes(16)
+    
+    cipher.reset(iv)
+    
     cipher.setkey("1234567890123456", 128, PolarSSL::Cipher::OPERATION_ENCRYPT)
     cipher.update("Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Sed posuere consectetur est at lobortis. Curabitur blandit tempus porttitor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed posuere consectetur est at lobortis.")
 
