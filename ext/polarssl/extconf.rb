73c164885b383878037df54827a70cdd3e974345
Michiel Sikkes <michiel@firmhouse.com>
2013-09-23 10:43:07 +0200

    Added header and lib dir compile options so you can configure the polarssl library on compilation

--- ext/polarssl/extconf.rb
+++ ext/polarssl/extconf.rb
@@ -2,6 +2,39 @@ require 'mkmf'
 
 $CFLAGS = "-g -Wall -Wdeclaration-after-statement " + $CFLAGS
 
+LIBDIR      = RbConfig::CONFIG['libdir']
+INCLUDEDIR  = RbConfig::CONFIG['includedir']
+
+HEADER_DIRS = [
+  # First search /opt/local for macports
+  '/opt/local/include',
+
+  # Then search /usr/local for people that installed from source
+  '/usr/local/include',
+
+  # Check the ruby install locations
+  INCLUDEDIR,
+
+  # Finally fall back to /usr
+  '/usr/include',
+]
+
+LIB_DIRS = [
+  # First search /opt/local for macports
+  '/opt/local/lib',
+
+  # Then search /usr/local for people that installed from source
+  '/usr/local/lib',
+
+  # Check the ruby install locations
+  LIBDIR,
+
+  # Finally fall back to /usr
+  '/usr/lib',
+]
+
+dir_config('polarssl', HEADER_DIRS, LIB_DIRS)
+
 unless find_header('polarssl/entropy.h')
   abort "libpolarssl is missing. please install libpolarssl"
 end
